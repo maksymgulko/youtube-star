a792a7f6b5774d8ef1242989a6e4a63bc8b2a289
maqss <maksymgulko@gmail.com>
2024-12-11 22:31:00 +0100

    tariffs 1

--- src/components/Tariffs/Tariffs.jsx
+++ src/components/Tariffs/Tariffs.jsx
@@ -1,14 +1,81 @@
 import s from "./Tariffs.module.css";
+import image from "../../assets/20241022225030_1.jpg";
 
 const Tariffs = () => {
   return (
     <div className={s.container}>
       <h2 className={s.title}>Пакети</h2>
       <div className={s.tariffs}>
-        <ul className="tariffsList">
-          <li className="tariffItem"></li>
-          <li className="tariffItem"></li>
-          <li className="tariffItem"></li>
+        <ul className={s.tariffsList}>
+          <li className={s.tariffItem}>
+            <img className={s.tariffImg} src={image} alt="placeholder" />
+            <div className={s.tariffText}>
+              <h3>Start</h3>
+              <p className={s.desc}>
+                Для всіх, хто хоче самостійно створити YouTube канал та
+                спробувати себе в ролі ютубера.
+              </p>
+              <p>
+                <span style={{ fontWeight: "bold" }}>Тривалість курсу</span>: 3
+                модулі (8 уроків)
+              </p>
+              <p
+                style={{
+                  textDecoration: "line-through",
+                  textDecorationColor: "red",
+                }}
+              >
+                1200 UAH
+              </p>
+              <p>890 UAH</p>
+            </div>
+          </li>
+          <li className={s.tariffItem}>
+            <img className={s.tariffImg} src={image} alt="" />
+            <div className={s.tariffText}>
+              <h3>Basic</h3>
+              <p className={s.desc}>
+                Для тих, хто намірений не лише спробувати себе в ролі ютубера,
+                а, може, й навтіь заробити на цьому.
+              </p>
+              <p>
+                <span style={{ fontWeight: "bold" }}>Тривалість курсу</span>: 3
+                модулі (10 уроків та деякі корисні матеріали)
+              </p>
+              <p
+                style={{
+                  textDecoration: "line-through",
+                  textDecorationColor: "red",
+                }}
+              >
+                2800 UAH
+              </p>
+              <p>2490 UAH</p>
+            </div>
+          </li>
+          <li className={s.tariffItem}>
+            <img className={s.tariffImg} src={image} alt="" />
+            <div className={s.tariffText}>
+              <h3>Advanced</h3>
+              <p className={s.desc}>
+                Для тих, хто дійсно хоче стати професійним контент-мейкером та
+                зробити це своїм основним видом діяльності.
+              </p>
+              <p>
+                <span style={{ fontWeight: "bold" }}>Тривалість курсу</span>: 3
+                модулі (11 уроків та багато інших корисних матеріалів)
+              </p>
+              <p
+                style={{
+                  textDecoration: "line-through",
+                  textDecorationColor: "red",
+                }}
+              >
+                5000 UAH
+              </p>
+              <p>4290 UAH</p>
+            </div>
+          </li>
         </ul>
       </div>
     </div>
